c8f1d8c05911e267bf6fdfe4af318a4f8e2da115
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-11-06 14:33:00 +0000

    feat: create a Spinner Component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div
+        class="spinner-container"
+    >
+        <div class="spiner" />
+    </div>
+</template>
+
+<style scoped>
+.spinner-container {
+  @apply flex justify-center items-center w-72 h-72
+}
+
+.spiner {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  border-left-color: #09f;
+
+  animation: spin 1s ease infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
