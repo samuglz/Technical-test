c7e77431376f1156dc35f02d46e44b0bc7394ae5
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-11-06 14:33:23 +0000

    feat: create ImageCard Component

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,61 @@
+<template>
+    <figure class="card">
+        <img
+            v-show="isLoad"
+            :src="src"
+            :alt="alt"
+            class="imgCard"
+            :class="{
+                'fadeIn': isLoad
+            }"
+            @load="handleLoad"
+        >
+        <Spinner v-show="!isLoad" />
+    </figure>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue';
+import Spinner from '@/components/Spinner.vue';
+
+const isLoad = ref(false);
+
+const handleLoad = () => {
+    isLoad.value = true;
+};
+
+defineProps({
+    src: {
+        type: String,
+        required: true
+    },
+    alt: {
+        type: String,
+        required: true
+    }
+});
+</script>
+
+<style scoped>
+.card{
+  @apply cursor-pointer md:w-72
+}
+
+.imgCard{
+  @apply rounded-md shadow-md md:w-72
+}
+
+.fadeIn{
+  animation: fadein 1.5s;
+}
+
+@keyframes fadein {
+  0%{
+    opacity: 0;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+
+</style>
